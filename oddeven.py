314bdaa4a51b6e853d4d02363bf42831f354c2c9
Gi Soong Chee <gi.soongchee@dhs.sg>
2022-08-24 09:41:40 +0800

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,3 +1,22 @@
+# Write a Boolean function is_odd(num) to return True if a positive integer num is odd 
+# and even if num is even
+
+def is_odd(num):
+    ''' Boolean function to return True if odd and False if even
+    '''
+    if num % 2 == 1:
+        return True
+    else: # even
+        return False
+
+    # Alternate one-liner:
+    # return num % 2 == 1
+
+# main
+print(is_odd(1))
+print(is_odd(888))
+    
+# Previously:
 # Write a function to determine if a positive integer is odd or even
 
 def oddeven(number):
