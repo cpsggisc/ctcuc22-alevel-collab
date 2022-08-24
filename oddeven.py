1f231755312131260faac7186d89823a4bac7237
jjcheung <94059335+jjcheung@users.noreply.github.com>
2022-08-24 09:36:26 +0800

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,4 +1,4 @@
 # Write a function to determine if a positive integer is odd or even
 
 def oddeven(number):
-    return "even" if num % 2 == 0 else "odd" 
+    return "even" if number % 2 == 0 else "odd" 
